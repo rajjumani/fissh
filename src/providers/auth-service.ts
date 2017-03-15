0d82c751d6906dd88e6db9491b5c6a068c43d8a4
Dhruv Thakkar <thakkardhruv440@gmail.com>
2017-03-15 18:18:41 +0530

    Login Module Complete

--- src/providers/auth-service.ts
+++ src/providers/auth-service.ts
@@ -5,14 +5,40 @@ import {Observable} from 'rxjs/Observable';
 
 
 export class User {
+ user_id : number;
+ profile_id : number;
  name: string;
+ fullname : string;
  email: string;
+ address : string;
+ mobile : string;
+ api_token : string;
 
- constructor(name: string, email: string) {
+ constructor(user_id: number, profile_id: number, name : string, fullname: string, email: string, address : string, mobile: string, api_token: string) {
+   this.user_id = user_id;
+   this.profile_id = profile_id;
    this.name = name;
+   this.fullname = fullname;
    this.email = email;
+   this.address = address;
+   this.mobile = mobile;
+   this.api_token = api_token;
  }
 }
+
+export class LoginError {
+ email : string;
+ password : string;
+ msg : string;
+
+ constructor(email : string, password : string, msg : string) {
+   this.email = email;
+   this.password = password;
+   this.msg = msg;
+ }
+}
+
+
 
 /*
   Generated class for the AuthService provider.
@@ -23,6 +49,7 @@ export class User {
 @Injectable()
 export class AuthService {
   currentUser: User;
+  loginError : LoginError;
   access : boolean;
 
   constructor() { 
@@ -47,23 +74,46 @@ export class AuthService {
                       })
             .then(data => {
 
-              console.log(data.status);
-              console.log(data.data); // data received by server
-              console.log(data.headers);
+              //console.log(data.status);
+              //console.log(data.data); // data received by server
+              //console.log(data.headers);
+
+              if(data.status == 200){
+                let datagot = JSON.parse(data.data);
+                if(datagot.error){
+                  
+                  //console.log(datagot.msg);
+
+                  let access = false;
+                  this.loginError = new LoginError("", "", datagot.msg);
+                  observer.next(access);
+                  observer.complete();
+                }
+                else{
+                  let access = true;
+                  this.currentUser = new User(datagot.user.id, datagot.user.userprofile.id, datagot.user.name, datagot.user.userprofile.fullname, datagot.user.email, datagot.user.userprofile.address, datagot.user.userprofile.mobile, datagot.api_token);
+                  observer.next(access);
+                  observer.complete();
+                }
+              }
 
             })
             .catch(error => {
 
-              console.log(error.status);
-              console.log(error.error); // error message as string
-              console.log(error.headers);
+              //console.log(error.status);
+              //console.log(error.error); // error message as string
+              //console.log(error.headers);
+
+              let datagot = JSON.parse(error.error);
+              //console.log(datagot.email);
+              //console.log(datagot.password);
+
+              let access = false;
+              this.loginError = new LoginError(datagot.email, datagot.password, "");
+              observer.next(access);
+              observer.complete();
 
             });
-        
-        let access = true;//(credentials.password === "admin1401" && credentials.email === "admin");
-        this.currentUser = new User('User', 'admin@gmail.com');
-        observer.next(access);
-        observer.complete();
       });
 
 
@@ -84,6 +134,10 @@ export class AuthService {
 
   public getUserInfo() : User {
     return this.currentUser;
+  }
+
+  public getLoginError() : LoginError {
+    return this.loginError;
   }
 
   public logout() {
